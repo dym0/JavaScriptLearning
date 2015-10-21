480f3bc8e132518a2e0262d362076422aa102f41
dym0 <damiankuzmicki1989@gmail.com>
2015-10-22 01:33:14 +0200

    phone book added in learn.js

--- js/learn.js
+++ js/learn.js
@@ -8,10 +8,23 @@ function hideCookieBlock(idtohide)
 	
 	
     //alert(data);
-	//cument.getElementById("test").innerHTML = document.cookie;
+	//cument.getElementById("test").textContent = document.cookie;
 	
 }
 
+function findLargets(first, second, third, fourth, fifth, sixth) {
+    var x = 0;
+
+    for (i = 0; i < arguments.length; i++) {
+        if (x < arguments[i]) {
+            x = arguments[i];
+        }
+
+    }
+
+
+    // document.getElementById('test').textContent = x;
+}
 
 function checkCookie(testString)
  {
@@ -26,13 +39,151 @@ function checkCookie(testString)
  			cookStr += "<b>pozycja " + i + ": " + cookTab[i]+ " </b>"; 
 
  		}	
- 		/*TEST* document.getElementById('test2').innerHTML = "document.cookie != 0, " + document.cookie + " " + cookStr;*/
+ 		/*TEST* document.getElementById('test2').textContent = "document.cookie != 0, " + document.cookie + " " + cookStr;*/
  		document.getElementById('cookieBox').style.visibility = "hidden";
  	}
  	else
  	{
- 		/*TESTdocument.getElementById('test2').innerHTML = "document.cookie = 0, " + document.cookie; */
+ 		/*TESTdocument.getElementById('test2').textContent = "document.cookie = 0, " + document.cookie; */
  	}
  	
  	
  }
+
+var mapa = {
+    texturA: "black",
+    texturB: "gray",
+    width: "100px",
+    height: 5,
+    viewFinder: "_|_",
+    margin: 100,
+    draw: function () {
+
+
+
+
+
+        for (i = 0; i < 5; i++) {
+
+            var blackDiv = document.createElement("div");
+            var whiteDiv = document.createElement("div");
+            document.getElementsByTagName('body')[0].appendChild(blackDiv);
+            document.getElementsByTagName('body')[0].appendChild(whiteDiv);
+
+            blackDiv.className = "classblack";
+            blackDiv.style.cssFloat = "left";
+            blackDiv.style.backgroundColor = "black";
+
+
+
+            whiteDiv.className = "classwhite";
+            whiteDiv.style.cssFloat = "left";
+            whiteDiv.style.backgroundColor = "gray";
+
+        }
+
+
+
+
+    }
+};
+
+
+function findName(myName)  // find your name in text and copy it to table.
+{
+    
+    var text2 = "stibidibidin dongdong Damian, wrc 8 monia";
+
+    var hick=[];
+    
+   
+    for(i = 0; i < text2.length; i++)
+    {
+        if(text2[i] === myName[0])
+        { 
+           for(j=i; j < myName.length + i; j++)
+            {
+                hick.push(text2[j]);
+            }
+
+        }
+    }
+
+    if(hick.length == 0)
+    {
+
+        alert(myName +", your name dont exist in text.");
+    }
+
+    if(hick.length != 0)
+    {
+        alert(myName + ", your name exist in text");
+        alert(hick.length);
+    }
+
+ 
+}
+
+    /* for (i = 0; i < this.height; i++) {
+
+
+         for (j = 0; i < this.width; i++) {
+             document.getElementById
+         }
+
+     }*/
+
+ 
+/* Phone book */
+
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        printPerson(contacts[i]);
+    }
+}
+function search(lastName) {
+    var contactLength = contacts.length;
+    for (i = 0; i < contactLength; i++) {
+        if (lastName === contacts[i].lastName) {
+            printPerson(contacts[i]);
+        }
+    }
+}
+
+function add(firstName, lastName, email, phoneNumber) {
+
+    contacts[contacts.length] = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phoneNumber: phoneNumber,
+    };
+}
+
+add("Zbychu", "Kubina", "zbychu.kubina@gmail.com", "0000202020202");
+
+list();
+
+
+/* PHone Book end */ 
